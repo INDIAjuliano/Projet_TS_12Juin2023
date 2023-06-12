fab2b3ad9e6b64ac1b54efd514dab0be5a09026c
INDIAMANITRIMO juliano <juliano.gasii1@gmail.com>
2023-06-12 20:16:09 +1300

    first depots

--- script.js
+++ script.js
@@ -1,55 +1,72 @@
-$(document).ready(function() {
-  $('.owl-carousel').owlCarousel({
-    center: true,
-    loop: true,
-    nav: true,
-    items: 5,
-    navText: ['<h1><</h1>','<h1>></h1>'],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 1,
-      },
-      990: {
-        items: 2,
-      }
+$('.owl-carousel').owlCarousel({
+  center: true,
+  loop: true,
+  nav: true,
+  items: 5,
+  navText: ['<h1 style="outline: none;"><</h1>','<h1 style="outline: none;">></h1>'],
+  responsive:{
+    0:{
+      items: 1,
     },
-    onInitialized: coverFlowEfx,
-    onTranslate: coverFlowEfx,
-  });
-
-  function coverFlowEfx(e) {
-    if ($('.owl-dots')) {
-      $('.owl-dots').remove();
+    768:{
+      items: 1,
+    },
+    990:{
+      items: 2,
     }
-    idx = e.item.index;
-    $('.owl-item.big').removeClass('big');
-    $('.owl-item.medium').removeClass('medium');
-    $('.owl-item.small').removeClass('small');
-    $('.owl-item').eq(idx).addClass('big');
-    $('.owl-item').eq(idx - 2).addClass('small');
-    $('.owl-item').eq(idx + 1).addClass('small');
-    $('.owl-item').eq(idx + 1).addClass('small');
+  },
+  onInitialized: coverFlowEfx,
+  onTranslate: coverFlowEfx,
+});
 
-    // Activation des divs correspondants
-    $('.div1, .div2, .div3').removeClass('active');
-    $('.div' + (idx + 1)).addClass('active');
+function coverFlowEfx(e){
+  if ($('.owl-dots')) {
+    $('.owl-dots').remove();
   }
+  idx = e.item.index;
+  $('.owl-item.big').removeClass('big');
+  $('.owl-item.medium').removeClass('medium');
+  $('.owl-item.small').removeClass('small');
+  $('.owl-item').eq(idx).addClass('big');
+  $('.owl-item').eq(idx - 2).addClass('small');
+  $('.owl-item').eq(idx + 1).addClass('small');
+  $('.owl-item').eq(idx + 1).addClass('small');
+}
+
+
+// var events = document.querySelectorAll('.event');
+// var currentEventIndex = 0;
+
+// // Affiche le premier événement
+// events[currentEventIndex].classList.add('active');
+
+// function nextEvent() {
+//   events[currentEventIndex].classList.remove('active');
+  
+//   // Passage à l'événement suivant
+//   currentEventIndex++;
+  
+//   // Si on a atteint le dernier événement, revenir au premier
+//   if (currentEventIndex >= events.length) {
+//     currentEventIndex = 0;
+//   }
+  
+//   events[currentEventIndex].classList.add('active');
+// }
+
+var click = false;
+
+$('#play-carousel').click(function(evt) {
+  click = !click;
+  if(click){
+    $('.status').html('Autoplay: ON');
+    $('.owl-carousel').trigger('play.owl.autoplay', [1000, 1000]);
+    $(this).html("Our premium food");
+  } else {
+    $('.owl-carousel').trigger('stop.owl.autoplay');
+    $(this).html("Our premium food");
+    $('.status').html('Autoplay: OFF');
+  }
+
 });
 
-$(document).ready(function() {
-  // ... Existing code
-
-  // Click event handler for displaying divs
-  $('.owl-item').click(function() {
-    var divIndex = $(this).index();
-
-    // Remove active class from all divs
-    $('.div1, .div2, .div3').removeClass('active');
-
-    // Add active class to the clicked div
-    $('.div' + (divIndex + 1)).addClass('active');
-  });
-});
